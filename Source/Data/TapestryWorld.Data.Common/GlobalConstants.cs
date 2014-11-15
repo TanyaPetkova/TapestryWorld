85c8bd4fa1cc76c3c96d831cc30f79cab73117ff
TanyaPetkova <taniapetkova@abv.bg>
2014-11-15 22:47:13 +0200

    Added GlobalConstants class in TapestryWorld.Data.Common.csproj

---
+++ Source/Data/TapestryWorld.Data.Common/GlobalConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace TapestryWorld.Data.Common
+{
+    using System.Web;
+
+    public class GlobalConstants
+    {
+        public const string AdminRole = "Admin";
+        public const string Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        public const int PageSize = 3;
+    }
+}
